5f52f18937436bdbbacd6dd8bd36a203f28e0c22
Kirill Andreev <kirill.andreev@klarna.com>
2023-12-01 13:09:40 +0100

    Day 1 refactor

--- src/Day1.kt
+++ src/Day1.kt
@@ -3,23 +3,27 @@ fun main() {
         readInput("Day1")
     }
 
-    fun part1(): Int = input.sumOf { line -> line.first { it.isDigit() }.digitToInt() * 10 + line.last { it.isDigit() }.digitToInt() }
+    fun getCalibration(digits: List<Int>): Int = digits.first() * 10 + digits.last()
+    fun toDigits(s: String) = s.filter { it.isDigit() }.map { it.digitToInt() }
 
-    val digitWords = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
-    fun part2(): Int = input.sumOf { line ->
-        val firstDigit: Int
-        val secondDigit: Int
-
-        val firstDigitIdx = line.indexOfFirst { it.isDigit() }
-        val firstWordDigit = digitWords.map { line.indexOf(it) }.withIndex().filter { it.value >= 0 }.minByOrNull { it.value } ?: IndexedValue(Int.MAX_VALUE, Int.MAX_VALUE)
-        firstDigit = if (firstDigitIdx < firstWordDigit.value) line[firstDigitIdx].digitToInt() else firstWordDigit.index + 1
-
-        val secondDigitIdx = line.indexOfLast { it.isDigit() }
-        val secondWordDigit = digitWords.map { line.lastIndexOf(it) }.withIndex().filter { it.value >= 0 }.maxByOrNull { it.value } ?: IndexedValue(Int.MIN_VALUE, Int.MIN_VALUE)
-        secondDigit = if (secondDigitIdx > secondWordDigit.value) line[secondDigitIdx].digitToInt() else secondWordDigit.index + 1
-
-        firstDigit * 10 + secondDigit
+    val digitWords = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine").mapIndexed { i, s -> s to i + 1 }.toMap()
+    fun toDigitsWithWords(s: String): List<Int> {
+        val digits = mutableListOf<Int>()
+        var i = 0
+        while (i < s.length) {
+            if (s[i].isDigit()) {
+                digits.add(s[i].digitToInt())
+                i++
+                continue
+            }
+            digitWords.entries.firstOrNull { s.startsWith(it.key, i) }?.also { digits.add(it.value) }
+            i++
+        }
+        return digits
     }
+
+    fun part1(): Int = input.map { toDigits(it) }.sumOf { getCalibration(it) }
+    fun part2(): Int = input.map { toDigitsWithWords(it) }.sumOf { getCalibration(it) }
 
     measure { part1() }
     measure { part2() }
