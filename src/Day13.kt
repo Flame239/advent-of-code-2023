106023e9269c5af52e555ea5e6e30470e2f0ac64
Kirill Andreev <kirill.andreev@klarna.com>
2024-02-05 20:28:42 +0100

    day 13 part 2 + refactor

--- src/Day13.kt
+++ src/Day13.kt
@@ -5,29 +5,61 @@ fun main() {
         readInput("Day13").splitBy { it.isEmpty() }
     }
 
+    fun rowEq(p: List<String>, i1: Int, i2: Int): Boolean = p[i1] == p[i2]
+    fun colEq(p: List<String>, j1: Int, j2: Int): Boolean = p.indices.all { i -> p[i][j1] == p[i][j2] }
+
     fun findHorizontalReflection(p: List<String>): Int {
-        outer@ for (i in 0..<p.size - 1) {
-            if (p[i] == p[i + 1]) {
-                val size = min(i + 1, p.size - (i + 1)) - 1
-                for (step in 1..size) {
-                    if (p[i - step] != p[i + 1 + step]) continue@outer
-                }
-                return i + 1
+        val n = p.size
+        outer@ for (i in 0..<n - 1) {
+            val width = min(i + 1, n - (i + 1))
+            for (w in 0 until width) {
+                if (!rowEq(p, i - w, i + 1 + w)) continue@outer
             }
+            return i + 1
         }
         return 0
     }
 
-    fun colEq(p: List<String>, j1: Int, j2: Int): Boolean = p.indices.all { i -> p[i][j1] == p[i][j2] }
-
     fun findVerticalReflection(p: List<String>): Int {
         val m = p[0].length
         outer@ for (j in 0..<m - 1) {
-            val size = min(j + 1, m - (j + 1))
-            for (step in 0 until size) {
-                if (!colEq(p, j - step, j + 1 + step)) continue@outer
+            val width = min(j + 1, m - (j + 1))
+            for (w in 0 until width) {
+                if (!colEq(p, j - w, j + 1 + w)) continue@outer
             }
             return j + 1
+        }
+        return 0
+    }
+
+    fun colDiff(p: List<String>, j1: Int, j2: Int): Int = p.indices.count { i -> p[i][j1] != p[i][j2] }
+    fun rowDiff(p: List<String>, i1: Int, i2: Int): Int = p[0].indices.count { j -> p[i1][j] != p[i2][j] }
+
+    fun findVerticalReflectionWithSmudge(p: List<String>): Int {
+        val m = p[0].length
+        outer@ for (j in 0..<m - 1) {
+            var diff = 0
+            val width = min(j + 1, m - (j + 1))
+            for (w in 0 until width) {
+                diff += colDiff(p, j - w, j + 1 + w)
+                if (diff > 1) continue@outer
+            }
+            if (diff == 1) return j + 1
+        }
+        return 0
+    }
+
+
+    fun findHorizontalReflectionWithSmudge(p: List<String>): Int {
+        val n = p.size
+        outer@ for (i in 0..<n - 1) {
+            var diff = 0
+            val width = min(i + 1, n - (i + 1))
+            for (w in 0 until width) {
+                diff += rowDiff(p, i - w, i + 1 + w)
+                if (diff > 1) continue@outer
+            }
+            if (diff == 1) return i + 1
         }
         return 0
     }
@@ -37,7 +69,7 @@ fun main() {
     }
 
     fun part2(): Int {
-        return patterns.size
+        return patterns.sumOf { p -> 100 * findHorizontalReflectionWithSmudge(p) + findVerticalReflectionWithSmudge(p) }
     }
 
     measure { part1() }
