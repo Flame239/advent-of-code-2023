16243d4bcfb6edf4be50d3e70aefb06bc54d3a64
Kirill Andreev <kirill.andreev@klarna.com>
2024-02-07 14:39:07 +0100

    day 18

--- src/Day18.kt
+++ src/Day18.kt
@@ -1,50 +1,44 @@
-import kotlin.math.abs
+import kotlin.math.absoluteValue
 
 fun main() {
-    data class Dig(val dir: Dir, val len: Int, val color: String)
-    data class Coord(val x: Int, val y: Int)
+    val letterToDir = mapOf("R" to DIR_RIGHT, "U" to DIR_UP, "L" to DIR_LEFT, "D" to DIR_DOWN)
+    val hexToDir = mapOf('0' to DIR_RIGHT, '3' to DIR_UP, '2' to DIR_LEFT, '1' to DIR_DOWN)
 
-    val dirLetters = mapOf("R" to DIR_RIGHT, "U" to DIR_UP, "L" to DIR_LEFT, "D" to DIR_DOWN)
-
-    val plan = readInput("Day18-ex").map { l -> l.split(" ").let { Dig(dirLetters[it[0]]!!, it[1].toInt(), it[2]) } }
-
-    fun shoelace(coords: List<Coord>): Int {
-        val n = coords.size
-        var area = 0
-
-        var j: Int = n - 1
-        for (i in 0 until n) {
-            area += (coords[j].x + coords[i].x) * (coords[j].y - coords[i].y)
-            j = i
-        }
-
-        return abs(area / 2)
+    data class Dig(val dir: Dir, val len: Long)
+    data class DigInput(val dir: String, val len: Long, val color: String) {
+        fun normalDig() = Dig(letterToDir[dir]!!, len)
+        fun hexDig() = Dig(hexToDir[color[5]]!!, color.substring(0, 5).toLong(16))
     }
 
-//    fun area(coords: List<Coord>): Int {
-//        val byY: Map<Int, List<Coord>> = coords.groupBy { it.y }
-//        return byY.toList().sumOf { (y, pts) -> pts.sortedBy { it.x }.chunked(2).sumOf { it[1].x - it[0].x + 1 }.also { println("$y $it") } }
-//    }
-
-    fun part1(): Int {
-        var cur = Coord(0, 0)
-        val coords = mutableListOf<Coord>()
-        plan.forEach { d ->
-            cur = Coord(cur.x + d.len * d.dir.stepJ, cur.y + d.len * d.dir.stepI)
-            coords.add(cur)
-        }
-        coords.println()
-
-        coords.size.println()
-        shoelace(coords).println()
-        return -1
+    data class V2(val x: Long, val y: Long) {
+        fun shift(dir: Dir, len: Long) = V2(x + len * dir.stepJ, y + len * dir.stepI)
     }
 
-    fun part2(): Int {
-        return plan.size
+    val planInput = readInput("Day18").map { l -> l.split(" ").let { DigInput(it[0], it[1].toLong(), it[2].substring(2, 8)) } }
+
+
+    fun shoelace(coords: List<V2>): Long = coords
+            .zipWithNext { c1, c2 ->
+                c1.x * c2.y - c1.y * c2.x
+            }.sum().absoluteValue / 2
+
+    fun interiorPoints(area: Long, border: Long): Long {
+        // Pick's theorem: Area = inside + border / 2 - 1
+        // we need: inside + border which is = Area + border / 2 + 1
+        // where Area could be calculated using shoelace formula
+        return area + border / 2 + 1
     }
 
-    shoelace(mutableListOf(Coord(0, 0), Coord(2, 0), Coord(2, 2), Coord(0, 2))).println()
+    fun volume(plan: List<Dig>): Long {
+        val coords = plan.scan(V2(0, 0)) { prev, d -> prev.shift(d.dir, d.len) }
+        val area = shoelace(coords)
+        val border = plan.sumOf(Dig::len)
+        return interiorPoints(area, border)
+    }
+
+    fun part1(): Long = volume(planInput.map(DigInput::normalDig))
+    fun part2(): Long = volume(planInput.map(DigInput::hexDig))
+
     measure { part1() }
     measure { part2() }
 }
